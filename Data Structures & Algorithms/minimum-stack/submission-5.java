50a87e8e10980dccf7b094d823334677fcb40e0b
jingyushim-cpu <jingyushim@gmail.com>
2026-09-01 01:02:09 -0400

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.java
@@ -0,0 +1,31 @@
+class MinStack {
+
+    private Deque<Integer> stack;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayDeque();
+        minStack = new ArrayDeque();    
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+
+        if(minStack.isEmpty() || minStack.peek() >= val) minStack.push(val);
+
+    }
+    
+    public void pop() {
+        if(stack.isEmpty()) return;
+        int val = stack.pop();
+        if(val == minStack.peek()) minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
