a2e5eea55580808552594bec95ead54e874ff9aa
jingyushim-cpu <jingyushim@gmail.com>
2026-09-07 10:06:06 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int i = 0; i < temperatures.length; i++){
+            while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
+                result[stack.peek()] = i - stack.pop();
+            }
+            
+            stack.push(i);
+
+            
+        }
+
+        return result;
+    }
+}
